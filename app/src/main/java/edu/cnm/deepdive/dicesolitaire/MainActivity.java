f1acc2c963cf2179306d0aabda2b6c4e5b748545
Mercedes <mercedes.nmx@gmail.com>
2020-06-02 15:58:29 -0600

    Refactored nested class to local class.

--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
@@ -62,6 +62,14 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void setupPlayControls() {
+    class RollerListener implements OnClickListener {
+      @Override
+      public void onClick(View v) {
+        Roll roll = new Roll(rng);
+        // TODO Display dice images.
+      }
+    }
+
     roller = findViewById(R.id.roller);
     // TODO Find & wire up dice ImageView objects.
     roller.setOnClickListener(new RollerListener());
@@ -82,14 +90,5 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
-  private class RollerListener implements OnClickListener {
-
-    @Override
-    public void onClick(View v) {
-      Roll roll = new Roll(rng);
-      // TODO Display dice images.
-    }
-
-  }
 
 }
